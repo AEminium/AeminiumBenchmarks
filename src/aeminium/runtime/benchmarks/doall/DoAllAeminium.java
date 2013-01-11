264055a95850d2d1dcb5b75121a1865fbba58c0a
Cristiano Gonçalves <matos@student.dei.uc.pt>
2013-01-11 14:44:16 +0000

    Add program DoAllAeminium from doall

---
+++ src/aeminium/runtime/benchmarks/doall/DoAllAeminium.java
@@ -0,0 +1,61 @@
+package aeminium.runtime.benchmarks.doall;
+
+import java.util.Collection;
+
+import aeminium.runtime.Body;
+import aeminium.runtime.Runtime;
+import aeminium.runtime.Task;
+import aeminium.runtime.implementations.Factory;
+
+public class DoAllAeminium {
+
+	public static Runtime rt;
+
+	// CONFIGURATIONS
+	public static int numberOfTasks;
+	public static int n;
+	public static int[] a;
+	public static int[] b;
+	public static int[] c;
+
+	public static void main(String args[]) {
+		n = Integer.parseInt(args[0]);
+		numberOfTasks = Integer.parseInt(args[1]);
+
+		a = new int[n];
+		b = new int[n];
+		c = new int[n];
+		
+		long initialTime = System.currentTimeMillis();
+		rt = Factory.getRuntime();
+		rt.init();
+
+		int i = 0;
+		while (i < numberOfTasks) {
+			doallTask(Runtime.NO_PARENT, Runtime.NO_DEPS, i);
+			i++;
+		}
+
+		rt.shutdown();
+
+		long finalTime = System.currentTimeMillis();
+		System.out.println("Time cost = " + (finalTime - initialTime) * 1.0 / 1000);
+
+	}
+
+	private static Task doallTask(Task current, Collection<Task> prev, final int tid) {
+		Task task = rt.createNonBlockingTask(new Body() {
+			@Override
+			public void execute(Runtime rt, Task current) {
+				int i = tid * n / numberOfTasks;
+				while (i < (tid + 1) * n / numberOfTasks) {
+					c[i] = a[i] + b[i];
+					i++;
+				}
+			}
+		}, Runtime.NO_HINTS);
+		rt.schedule(task, current, prev);
+		return task;
+	}
+
+}
