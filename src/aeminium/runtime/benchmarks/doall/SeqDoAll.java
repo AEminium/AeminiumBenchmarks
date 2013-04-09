d12e9b963218d277faa3af460e1e651fdc0c871d
Alcides Fonseca <me@alcidesfonseca.com>
2013-04-09 22:18:52 +0100

    Added doall experiment

---
+++ src/aeminium/runtime/benchmarks/doall/SeqDoAll.java
@@ -0,0 +1,16 @@
+package aeminium.runtime.benchmarks.doall;
+
+public class SeqDoAll {
+	
+	public static void main(String[] args) {
+		int size = 1000000;
+		if (args.length > 0)
+			size = Integer.parseInt(args[0]);
+
+		double[] c = new double[size];
+		
+		for (int i = 0; i < size; i++) {
+			c[i] = Math.sqrt(i) / Math.sin(i);
+		}
+	}
+}
